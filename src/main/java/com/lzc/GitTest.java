fe29f41863410cd1d87a2d9093c7c143d98fcd35
李志川 <1474609635@qq.com>
2022-10-05 17:07:03 +0800

    second commit

--- src/main/java/com/lzc/GitTest.java
+++ src/main/java/com/lzc/GitTest.java
@@ -8,6 +8,7 @@ package com.lzc;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 
 }
