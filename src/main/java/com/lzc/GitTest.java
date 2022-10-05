8f1a4cbe6cb36f3ac71edc680426c7bd70bce95a
李志川 <1474609635@qq.com>
2022-10-05 17:05:30 +0800

    first commit

---
+++ src/main/java/com/lzc/GitTest.java
@@ -0,0 +1,13 @@
+package com.lzc;
+
+/**
+ * @Auther: 某某
+ * @Date: 2022/10/5 17:00
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
