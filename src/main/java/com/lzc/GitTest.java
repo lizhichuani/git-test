b39b98a58b559d3be233edbf180f65d2fc72dd70
lizhichuani <113006758+lizhichuani@users.noreply.github.com>
2022-10-05 17:39:41 +0800

    pull commit

--- src/main/java/com/lzc/GitTest.java
+++ src/main/java/com/lzc/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("master test！");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 
 }
